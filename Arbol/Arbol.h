218c2b3168552191989df11e685f9d00ee33231f
Ivan GC <ivannavi.13@hotmail.com>
2019-11-24 23:04:13 -0600

    Se actualizo Arbol

---
+++ Arbol/Arbol.h
@@ -0,0 +1,24 @@
+struct Arbol{
+	int dato;
+	struct Arbol *izq;
+	struct Arbol *der;
+};
+
+struct Arbol *AgregarElemento(struct Arbol *, int dato);
+struct Arbol *EliminarElemento(struct Arbol *, int dato);
+void BuscarElemento(struct Arbol *,int dato);
+void PRE(struct Arbol *);
+void IN(struct Arbol *);
+void POST(struct Arbol *);
+
+int EsHoja(struct Arbol *raiz)
+{
+	return !raiz -> izq && !raiz -> der;
+}
+int Vacio(struct Arbol *raiz)
+{
+	return raiz == NULL;
+}
+
+
+
